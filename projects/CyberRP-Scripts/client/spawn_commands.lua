f276fd43e13cd2dfcbce8b24591149ab7bc7a479
Bobby Falero <bobbyfalero@gmail.com>
2025-05-27 01:31:41 -0500

    Update spawn_commands.lua
    Replace spawn_commands.lua with upgraded version using animation and scenario-based NPCs

--- projects/CyberRP-Scripts/client/spawn_commands.lua
+++ projects/CyberRP-Scripts/client/spawn_commands.lua
@@ -1,31 +1,53 @@
 -- client/spawn_commands.lua
 
-local function SpawnCharacterModel(model)
-  local playerPed = PlayerPedId()
+-- Generic ped spawner with animation
+function SpawnCharacterWithAnimation(model, scenario)
   local modelHash = GetHashKey(model)
   RequestModel(modelHash)
   while not HasModelLoaded(modelHash) do Wait(0) end
-  SetPlayerModel(PlayerId(), modelHash)
-  SetModelAsNoLongerNeeded(modelHash)
+
+  local coords = GetEntityCoords(PlayerPedId())
+  local ped = CreatePed(4, modelHash, coords.x + 1.5, coords.y, coords.z, 0.0, true, false)
+  SetEntityAsMissionEntity(ped, true, true)
+
+  if scenario then
+    TaskStartScenarioInPlace(ped, scenario, 0, true)
+  end
 end
 
-RegisterCommand("spawnlola", function() SpawnCharacterModel("mp_f_freemode_01") end)
-RegisterCommand("spawnluis", function() SpawnCharacterModel("mp_m_freemode_01") end)
-RegisterCommand("spawnlitloo", function() SpawnCharacterModel("a_m_y_skater_01") end)
-RegisterCommand("spawnlitg", function() SpawnCharacterModel("ig_floyd") end) -- or custom
-RegisterCommand("spawncoco", function()
+-- Register character commands
+RegisterCommand("spawn_lola", function()
+  SpawnCharacterWithAnimation("mp_f_freemode_01", "WORLD_HUMAN_STAND_IMPATIENT")
+end)
+
+RegisterCommand("spawn_luis", function()
+  SpawnCharacterWithAnimation("mp_m_freemode_01", "WORLD_HUMAN_MUSCLE_FLEX")
+end)
+
+RegisterCommand("spawn_litloo", function()
+  SpawnCharacterWithAnimation("a_m_y_skater_01", "WORLD_HUMAN_HANG_OUT_STREET")
+end)
+
+RegisterCommand("spawn_litg", function()
+  SpawnCharacterWithAnimation("ig_floyd", "WORLD_HUMAN_DRINKING")
+end)
+
+RegisterCommand("spawn_coco", function()
   local hash = GetHashKey("a_c_rottweiler")
   RequestModel(hash)
   while not HasModelLoaded(hash) do Wait(0) end
+
   local coords = GetEntityCoords(PlayerPedId())
-  local pet = CreatePed(28, hash, coords.x + 2, coords.y, coords.z, 0.0, true, false)
+  local pet = CreatePed(28, hash, coords.x + 2.0, coords.y, coords.z, 0.0, true, false)
   SetEntityAsMissionEntity(pet, true, true)
+  TaskStartScenarioInPlace(pet, "WORLD_DOG_SITTING", 0, true)
 end)
 
-RegisterCommand("spawnimpala", function()
+RegisterCommand("spawn_impala", function()
   local hash = GetHashKey("impala64")
   RequestModel(hash)
   while not HasModelLoaded(hash) do Wait(0) end
+
   local coords = GetEntityCoords(PlayerPedId())
   local vehicle = CreateVehicle(hash, coords.x + 5, coords.y, coords.z, 0.0, true, false)
   SetVehicleNumberPlateText(vehicle, "LUIS64")
