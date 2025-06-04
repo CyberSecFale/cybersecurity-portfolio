0992a82f48cf308621a285c674530ac86efe9081
Bobby Falero <bobbyfalero@gmail.com>
2025-06-04 12:56:02 -0500

    Create fxmanifest.lua
    🧪 Add fxmanifest for sunglasses module

---
+++ projects/CyberRP-Scripts/modules/sunglasses/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- fxmanifest.lua for sunglasses module
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bobby Falero (CyberWolf001)'
+description 'Sunglasses accessory module for modular FiveM characters'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+
+lua54 'yes'
