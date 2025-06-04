306513d83e0b219e59e4557bc356ab82f031f8ad
Bobby Falero <bobbyfalero@gmail.com>
2025-06-04 13:27:42 -0500

    Create fxmanifest.lua
    🧪 Add fxmanifest for AR-sunglasses module

---
+++ projects/CyberRP-Scripts/modules/modules/ar_glasses/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- fxmanifest.lua for AR Glasses Module
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bobby Falero (CyberWolf001)'
+description 'Advanced Augmented Reality Glasses with immersive toggle features'
+version '1.0.0'
+
+client_script 'config.lua'
+client_script 'client.lua'
+
+lua54 'yes'
