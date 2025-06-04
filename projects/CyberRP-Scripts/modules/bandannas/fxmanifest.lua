adc93b12b40db653f4bf4f1f13cd523c0070f4c2
Bobby Falero <bobbyfalero@gmail.com>
2025-06-04 12:40:57 -0500

    Create fxmanifest.lua
    📦 Add fxmanifest for bandanna accessory module

---
+++ projects/CyberRP-Scripts/modules/bandannas/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- fxmanifest.lua for bandanna accessory module
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bobby Falero (CyberWolf001)'
+description 'Bandanna accessory system for modular NPC visuals'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+
+lua54 'yes'
