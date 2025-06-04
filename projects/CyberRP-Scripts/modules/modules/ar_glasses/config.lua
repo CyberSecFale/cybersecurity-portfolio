44a04da6624eba9fdaf43c26bc1af9f2785edd8d
Bobby Falero <bobbyfalero@gmail.com>
2025-06-04 13:30:30 -0500

    Create config.lua
    🧾 Add sunglasses config with AR-Threat Scan Integration
    Use GetPedAccuracy, IsPedArmed, or relationship group comparisons to determine hostile threat level
    
    Highlight hostiles in red using SetEntityAlpha, DrawMarker, or NUI outline shaders

---
+++ projects/CyberRP-Scripts/modules/modules/ar_glasses/config.lua
@@ -0,0 +1,9 @@
+-- config.lua for AR Glasses
+
+Config = Config or {}
+
+Config.ARGlassesVariants = {
+    { drawable = 25, texture = 4, tier = "standard" },
+    { drawable = 25, texture = 5, tier = "standard" },
+    { drawable = 26, texture = 0, tier = "premium" }
+}
