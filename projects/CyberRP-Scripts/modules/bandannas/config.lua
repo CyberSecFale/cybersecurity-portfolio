57d0787e538fd07836b9868b8e77b5eed162529d
Bobby Falero <bobbyfalero@gmail.com>
2025-06-04 12:39:38 -0500

    Create config.lua
    🎨 Add bandanna config with color variants

---
+++ projects/CyberRP-Scripts/modules/bandannas/config.lua
@@ -0,0 +1,12 @@
+-- config.lua for bandannas
+
+Config = Config or {}
+
+Config.Bandannas = {
+    red = { drawable = 0, texture = 0 },
+    blue = { drawable = 1, texture = 0 },
+    green = { drawable = 2, texture = 0 },
+    yellow = { drawable = 3, texture = 0 },
+    purple = { drawable = 4, texture = 0 },
+    blackwhite = { drawable = 5, texture = 0 } -- fallback
+}
