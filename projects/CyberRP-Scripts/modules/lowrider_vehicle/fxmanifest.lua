271076827fbb14a1b345b3c914114dae84fb703d
Bobby Falero <bobbyfalero@gmail.com>
2025-06-05 11:20:42 -0500

    Create fxmanifest.lua
    This manifest file defines the FiveM resource metadata for the Lowrider module. It includes:
    
    FX version compatibility (cerulean) and GTA5 targeting
    
    Author and description fields for module clarity in the resource list
    
    Declaration of the main client.lua script to be executed on the client side
    
    Easy plug-and-play integration with other modules under your CyberRP-Scripts resource tree
    
    This is required for the module to be recognized and correctly registered by the FiveM engine.

---
+++ projects/CyberRP-Scripts/modules/lowrider_vehicle/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bobby [Abbiu] FⱯLERO'
+description 'Enhanced Lowrider Vehicle Integration Script for FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
